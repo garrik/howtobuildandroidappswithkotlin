46261babb6f7a06bfa8d1b2e3bd3490cc5de7b31
garrik <federico.perotto@tiscali.it>
2022-08-25 13:06:19 +0200

    Exercise 1.03: Configuring the Android Manifest Internet Permission

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
@@ -2,10 +2,14 @@ package it.garrik.howtobuildandroidappswithkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        setContentView(webView)
+        webView.loadUrl("https://www.google.com")
     }
 }
