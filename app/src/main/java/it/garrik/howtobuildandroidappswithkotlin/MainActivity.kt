51995475a2b03ad329a32579235d814071f45379
garrik <federico.perotto@tiscali.it>
2022-11-23 23:57:09 +0100

    step 1 to 3

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
@@ -3,6 +3,10 @@ package it.garrik.howtobuildandroidappswithkotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+interface StarSignListener {
+    fun onSelected(id: Int)
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
