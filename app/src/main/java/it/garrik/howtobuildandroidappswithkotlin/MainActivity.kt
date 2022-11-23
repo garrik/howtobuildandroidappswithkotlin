4c9c6b5c1587448cc237ffc6566337470a81a2a7
garrik <federico.perotto@tiscali.it>
2022-11-23 23:38:15 +0100

    step 1 to 7

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
@@ -2,10 +2,26 @@ package it.garrik.howtobuildandroidappswithkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentContainerView
 
-class MainActivity : AppCompatActivity() {
+interface StarSignListener {
+    fun onSelected(id: Int)
+}
+
+class MainActivity : AppCompatActivity(), StarSignListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            findViewById<FragmentContainerView>(R.id.fragment_container)?.let { frameLayout ->
+                val listFragment = ListFragment()
+                supportFragmentManager.beginTransaction()
+                    .add(frameLayout.id, listFragment).commit()
+            }
+        }
+    }
+
+    override fun onSelected(id: Int) {
     }
 }
