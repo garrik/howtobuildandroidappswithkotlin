2bc3fb80d0c8b2061a64d416adcd515916fd404f
garrik <federico.perotto@tiscali.it>
2022-12-04 10:16:20 +0100

    step 1 to 23

---
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/api/TheCatApiService.kt
@@ -0,0 +1,13 @@
+package it.garrik.howtobuildandroidappswithkotlin.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TheCatApiService {
+    @GET("images/search")
+    fun searchImages(
+        @Query("limit") limit: Int,
+        @Query("size") format: String
+    ) : Call<String>
+}
