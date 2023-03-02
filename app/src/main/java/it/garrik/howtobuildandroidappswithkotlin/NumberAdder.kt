c7b1ce83a3dd39697ae6eddc7d551da51dd5f2c9
garrik <federico.perotto@tiscali.it>
2023-03-02 22:53:31 +0100

    step 6

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/NumberAdder.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/NumberAdder.kt
@@ -8,12 +8,7 @@ class NumberAdder {
         if (n < 0) {
             throw InvalidNumberException
         }
-        var result = BigInteger.ZERO
-        for (i in 1..n){
-            result = result.plus(i.toBigInteger())
-        }
-        callback(result)
-
+        callback(n.toBigInteger().times((n.toBigInteger() + 1.toBigInteger())).divide(2.toBigInteger()))
     }
     object InvalidNumberException : Throwable()
 }
