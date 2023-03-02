ae2871c727ce08b9f73001442eca969095281b1d
garrik <federico.perotto@tiscali.it>
2023-03-03 00:19:57 +0100

    step 1 to 15

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/MainActivity.kt
@@ -2,10 +2,21 @@ package it.garrik.howtobuildandroidappswithkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textFormatter: TextFormatter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textFormatter = TextFormatter(NumberAdder(), applicationContext)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            textFormatter.getSumResult(findViewById<EditText>(R.id.edit_text).text.toString().toIntOrNull() ?: 0) {
+                findViewById<TextView>(R.id.text_view).text = it
+            }
+        }
     }
 }
