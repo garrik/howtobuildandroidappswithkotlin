464c5df8f5596f52f84667119210413c641ab3bc
garrik <federico.perotto@tiscali.it>
2023-03-06 00:37:02 +0100

    step 7, improve test speed

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/Adder.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/Adder.kt
@@ -2,10 +2,6 @@ package it.garrik.howtobuildandroidappswithkotlin
 
 class Adder {
     fun sum(n: Int): Long {
-        var result = 0L
-        for (i in 1..n) {
-            result += i
-        }
-        return result
+        return (n * (n.toLong() + 1)) / 2
     }
 }
