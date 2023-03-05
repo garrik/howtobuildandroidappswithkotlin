65b0fb8a9d29f2e6305e5ff1e7b734fac42125d5
garrik <federico.perotto@tiscali.it>
2023-03-06 00:34:45 +0100

    step 5 - 6, fix success scenario

--- app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/Adder.kt
+++ app/src/main/java/it/garrik/howtobuildandroidappswithkotlin/Adder.kt
@@ -1,5 +1,11 @@
 package it.garrik.howtobuildandroidappswithkotlin
 
 class Adder {
-    fun sum(n: Int): Int = 0
+    fun sum(n: Int): Long {
+        var result = 0L
+        for (i in 1..n) {
+            result += i
+        }
+        return result
+    }
 }
